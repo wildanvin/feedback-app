3b082d2bfdd5882773458decc658d9f8d9035c01
wildanvin <danvin.jim@gmail.com>
2021-12-16 02:02:27 -0500

    add FeedbackStats componenet

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -11,7 +11,7 @@ const FeedbackData = [
   },
   {
     id: 3,
-    rating: 8,
+    rating: 1,
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
   },
 ];
