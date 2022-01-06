a7baed94437e7b49f4210de925ab9c27d62c7149
wildanvin <danvin.jim@gmail.com>
2022-01-06 01:25:37 -0500

    Fade Animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,5 @@
+import { motion, AnimatePresence } from "framer-motion";
+
 import PropTypes from "prop-types";
 
 import FeedbackItem from "./FeedbackItem";
@@ -8,13 +10,35 @@ function FeedbackList({ feedback, handleDelete }) {
   }
   return (
     <div className="feedback-list">
-      {feedback.map((item) => {
-        return (
-          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-        );
-      })}
+      <AnimatePresence>
+        {feedback.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => {
+  //       return (
+  //         <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //       );
+  //     })}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
