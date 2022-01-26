b9be85751d29f62549262dd90f26b14150d4f013
wildanvin <danvin.jim@gmail.com>
2022-01-25 22:13:01 -0500

    remove empty object as parameter

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -6,7 +6,7 @@ import { useContext } from "react";
 
 import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackList({}) {
+function FeedbackList() {
   const { feedback } = useContext(FeedbackContext);
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet</p>;
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackStats({}) {
+function FeedbackStats() {
   const { feedback } = useContext(FeedbackContext);
   //Calculate ratings average
 
