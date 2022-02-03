814d991239e7633aba3454b99eff6b1d7b7256f2
wildanvin <danvin.jim@gmail.com>
2022-02-02 22:11:07 -0500

    Add Feedback And Setting a Proxy

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -14,9 +14,7 @@ export const FeedbackProvider = ({ children }) => {
 
   //Fetch Feedback
   const fetchFeedback = async () => {
-    const response = await fetch(
-      "http://localhost:5000/feedback?_sort=id&_order=desc"
-    );
+    const response = await fetch("/feedback?sort=id&_order=desc");
 
     const data = await response.json();
 
@@ -32,9 +30,18 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = +uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   //Update feedback item
